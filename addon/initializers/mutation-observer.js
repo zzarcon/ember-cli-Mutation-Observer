2f4ea2faed9a93b3f0e3e171b143b4f050706ccb
Hector Leon Zarco Garcia <hezarco@gmail.com>
2015-11-10 22:23:27 +0100

    Use environment flag in order to implement the Mixin in all Components

--- addon/initializers/mutation-observer.js
+++ addon/initializers/mutation-observer.js
@@ -1,8 +1,16 @@
+/**
+ * Simple initializer that implements the mutationObserverMixin 
+ * in all the Components. It uses a flag defined in the app 
+ * environment for it.
+ */
+
 import Ember from 'ember';
 import mutationObserver from '../mixins/mutation-observer';
 
-export function initialize(/* container, application */) {
-  //TODO: Check if the flag is true
+export function initialize(container, application) {
+  let config = container.lookupFactory('config:environment')
+  
+  if (!config.mutationObserverInjection) return;
   
   Ember.Component.reopen(mutationObserver);
 }
