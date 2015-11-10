6cbc673f5418a7d59e7b77e9ce8b035f7fcecea8
Hector Leon Zarco Garcia <hezarco@gmail.com>
2015-11-10 20:03:33 +0100

    Check browser support

--- addon/mixins/mutation-observer.js
+++ addon/mixins/mutation-observer.js
@@ -6,26 +6,27 @@
 
 import Ember from 'ember';
 
+const defaultMutationConfig = {
+  attributes: true,
+  childList: true,
+  characterData: true
+};
+const hasMutationSupport = !!window.MutationObserver;
+
 export default Ember.Mixin.create({
   mutationInstance: null,
   onMutation: null,
-  defaultMutationConfig: {
-    attributes: true,
-    childList: true,
-    characterData: true
-  },
-  mutationConfig: null, //User config
-
+  mutationConfig: null, 
+  
   setupObserver: Ember.on('didInsertElement', function() {
     var cb = this.get('onMutation');
-    if (Ember.typeOf(cb) !== 'function') return;
+    if (!hasMutationSupport || Ember.typeOf(cb) !== 'function') return;
 
     var target = this.$()[0];     
     var observer = new MutationObserver((mutations) => {
-      //TODO: return objects in an better way
-      cb(mutations);
+      cb(mutations); //TODO: return objects in an better way
     });
-    var config = Ember.merge(this.get('defaultMutationConfig'), this.get('mutationConfig'));
+    var config = Ember.merge(defaultMutationConfig, this.get('mutationConfig'));
 
     observer.observe(target, config);
      
