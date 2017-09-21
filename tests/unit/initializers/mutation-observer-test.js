f5a1a04373b52dc0ed740f444ea210799a1a1ead
David Govea <govea.d@gmail.com>
2017-09-21 14:56:09 -0700

    Add test for `mutationObserverInjection` config flag

--- tests/unit/initializers/mutation-observer-test.js
+++ tests/unit/initializers/mutation-observer-test.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import sinon from 'sinon';
 import { initialize } from 'dummy/initializers/mutation-observer';
+import mutationObserver from 'dummy/mixins/mutation-observer';
 import { module, test } from 'qunit';
 import destroyApp from '../../helpers/destroy-app';
 
@@ -16,14 +17,29 @@ module('Unit | Initializer | mutation-observer', {
   }
 });
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('it should not alter Ember.Component by default', function(assert) {
   const spy = sinon.spy(Ember.Component, 'reopen');
 
   initialize(this.application);
 
   // Without specific config, the mixin will not be injected in all Components
-  assert.notOk(spy.called);
+  assert.notOk(spy.called, 'Component was not altered');
+
+  Ember.Component.reopen.restore();
+});
+
+test('it reopens Ember.Component with `mutationObserverInjection` config flag', function(assert) {
+  this.application.register('config:environment', Ember.Object.create({
+    mutationObserverInjection: true
+  }));
+
+  const spy = sinon.spy(Ember.Component, 'reopen');
+
+  initialize(this.application);
+
+  // Without specific config, the mixin will not be injected in all Components
+  assert.ok(spy.calledOnce, 'Component was reopened due to config flag');
+  assert.ok(spy.calledWith(mutationObserver), 'Component received mutation mixin');
 
   Ember.Component.reopen.restore();
 });
